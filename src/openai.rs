84b1c35be8474d1f327f3cd1caa13c297419fcbe
Greg Hurrell <greg.hurrell@datadoghq.com>
2025-05-12 11:05:58 +0200

    chore: bump OpenAI model to GPT-4.1
    > GPT-4.1 is our flagship model for complex tasks. It is well suited for
    > problem solving across domains.
    
    https://platform.openai.com/docs/models/gpt-4.1
    
    Marketing spiel:
    
    > Alongside 4.1 mini and 4.1 nano, it makes up our latest flagship GPT
    > family, specifically trained for developer use-cases like coding,
    > instruction-following, and function-calling.
    >
    > GPT-4.1 represents a significant upgrade over GPT-4o, particularly for
    > real-world software engineering tasks—it achieves 55% on SWE-bench
    > Verified, compared to GPT-4o’s 33%. It’s notably more skilled at
    > frontend coding, providing clean diffs, structured responses, reliable
    > tool usage, and more.
    >
    > Get started with GPT-4.1
    >
    > • Largest context window: GPT-4.1 offers the largest context window
    >   of our models—supporting over 1 million tokens—and is able to
    >   better use that context with improved long-context comprehension.
    > • Lower costs and latency: GPT-4.1 pushes model performance
    >   forward at every point on the latency curve. These new models are
    >   26% cheaper than GPT-4o on median queries, and nano is our fastest
    >   and cheapest model ever. And, there's no extra charge for using long
    >   context—it’s just the normal token prices.
    > • Instruction following: GPT-4.1 follows instructions more
    >   reliably. On internal evals, GPT-4.1 outranks GPT-4o on tasks like
    >   format adherence, complying with negative instructions, and ordering.
    >
    > Thomson Reuters tested GPT‑4.1 with CoCounsel, their
    > professional-grade AI assistant for legal work. They reported a
    > 17% improvement in multi-document review accuracy compared to
    > GPT‑4o on internal long-context benchmarks. Carlyle used GPT‑4.1
    > to accurately extract granular financial data from multiple,
    > lengthy documents—including PDFs, Excel files, and other complex
    > formats—achieving a 50% improvement on retrieval tasks involving
    > large, data-dense documents.
    >
    > I’ve seen many developers adopt GPT-4.1 recently and recognize its
    > coding capabilities, instruction-following, speed, and price, so I
    > hope’ll you get a chance to try them out too! And as always, please
    > let us know what you think.

--- src/openai.rs
+++ src/openai.rs
@@ -4,7 +4,7 @@ use reqwest::header::{HeaderMap, HeaderValue};
 use reqwest::{Client, RequestBuilder};
 use serde::{Deserialize, Serialize};
 
-const MODEL: &str = "gpt-4o";
+const MODEL: &str = "gpt-4.1";
 pub fn get_request(api_key: &str, model: &str, request: ChatRequest) -> RequestBuilder {
     let mut messages = vec![];
     if model != "o1-preview" && model != "o1-mini" {
